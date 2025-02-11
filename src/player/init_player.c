404a2c4e52e31e728d66458ab1fea71079b2850c
Qrshh <adrienbesneux@outlook.fr>
2025-02-11 23:23:52 +0100

    Spwan angle is good

--- src/player/init_player.c
+++ src/player/init_player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_player.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mosmont <mosmont@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*   By: abesneux <abesneux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:43:56 by abesneux          #+#    #+#             */
-/*   Updated: 2025/02/11 22:43:53 by mosmont          ###   ########.fr       */
+/*   Updated: 2025/02/11 23:23:34 by abesneux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 void init_player(t_all **all, int i, int j)
 {
-    (*all)->player_pos.x = (j + 0.5) * TILE_SIZE; // Taille en pixels
+    (*all)->player_pos.x = (j + 0.5) * TILE_SIZE;  // Position centrée sur la case
     (*all)->player_pos.y = (i + 0.5) * TILE_SIZE;
-    (*all)->starting_dir = (*all)->map[i][j];
+    (*all)->starting_dir = (*all)->map[i][j];  // Direction de départ du joueur
 
+    // Définir l'angle du joueur en fonction de la direction
     if ((*all)->map[i][j] == 'N')
-        (*all)->player_angle = 0;  // Nord
+        (*all)->player_angle = 3 * M_PI / 2;  // Nord (regarde vers le haut)
     else if ((*all)->map[i][j] == 'E')
-        (*all)->player_angle = M_PI / 2;         // Est (vers la droite)
+        (*all)->player_angle = 0;  // Est (regarde vers la droite)
     else if ((*all)->map[i][j] == 'S')
-        (*all)->player_angle = 3 * M_PI / 2; // Sud (vers le bas)
+        (*all)->player_angle = M_PI / 2;  // Sud (regarde vers le bas)
     else if ((*all)->map[i][j] == 'W')
-        (*all)->player_angle = M_PI;     // Ouest (vers la gauche)
+        (*all)->player_angle = M_PI;  // Ouest (regarde vers la gauche)
 
-    (*all)->map[i][j] = '0'; // Remplace la position du joueur par un sol
+    // Remplacer la case du joueur par du sol
+    (*all)->map[i][j] = '0'; 
 }
+
