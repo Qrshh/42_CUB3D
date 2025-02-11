c769b6c9b1d0aee4bd0aa5f85850c59c3c793367
Azaleee <oshoffpro@gmail.com>
2025-02-11 22:44:43 +0100

    Deplacement fix

--- src/player/player.c
+++ src/player/player.c
@@ -3,32 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abesneux <abesneux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mosmont <mosmont@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 22:10:58 by mosmont           #+#    #+#             */
-/*   Updated: 2025/02/11 22:04:31 by mosmont          ###   ########.fr       */
+/*   Updated: 2025/02/11 22:41:02 by mosmont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 void move_forward(t_all *all)
-{
-    double next_x = all->player_pos.x - cos(all->player_angle) * MOV_SPEED;
-    double next_y = all->player_pos.y - sin(all->player_angle) * MOV_SPEED;
-    int map_x = (int)((next_x - COLLISION_MARGIN * cos(all->player_angle)) / TILE_SIZE);
-    int map_y = (int)((next_y - COLLISION_MARGIN * sin(all->player_angle)) / TILE_SIZE);
-
-    if (all->map[map_y][(int)(all->player_pos.x / TILE_SIZE)] != '1')
-        all->player_pos.y = next_y;
-    if (all->map[(int)(all->player_pos.y / TILE_SIZE)][map_x] != '1')
-        all->player_pos.x = next_x;
-
-    all->img->instances[0].x = all->player_pos.x;
-    all->img->instances[0].y = all->player_pos.y;
-}
-
-void move_backward(t_all *all)
 {
 	double next_x = all->player_pos.x + cos(all->player_angle) * MOV_SPEED;
     double next_y = all->player_pos.y + sin(all->player_angle) * MOV_SPEED;
@@ -40,8 +24,24 @@ void move_backward(t_all *all)
     if (all->map[(int)(all->player_pos.y / TILE_SIZE)][map_x] != '1')
         all->player_pos.x = next_x;
 
-    all->img->instances[0].x = all->player_pos.x;
-    all->img->instances[0].y = all->player_pos.y;
+    // all->img->instances[0].x = all->player_pos.x;
+    // all->img->instances[0].y = all->player_pos.y;
+}
+
+void move_backward(t_all *all)
+{
+    double next_x = all->player_pos.x - cos(all->player_angle) * MOV_SPEED;
+    double next_y = all->player_pos.y - sin(all->player_angle) * MOV_SPEED;
+    int map_x = (int)((next_x - COLLISION_MARGIN * cos(all->player_angle)) / TILE_SIZE);
+    int map_y = (int)((next_y - COLLISION_MARGIN * sin(all->player_angle)) / TILE_SIZE);
+
+    if (all->map[map_y][(int)(all->player_pos.x / TILE_SIZE)] != '1')
+        all->player_pos.y = next_y;
+    if (all->map[(int)(all->player_pos.y / TILE_SIZE)][map_x] != '1')
+        all->player_pos.x = next_x;
+
+    // all->img->instances[0].x = all->player_pos.x;
+    // all->img->instances[0].y = all->player_pos.y;
 }
 
 void move_left(t_all *all)
@@ -56,8 +56,8 @@ void move_left(t_all *all)
     if (all->map[(int)(all->player_pos.y / TILE_SIZE)][map_x] != '1')
         all->player_pos.x = next_x;
 
-    all->img->instances[0].x = all->player_pos.x;
-    all->img->instances[0].y = all->player_pos.y;
+    // all->img->instances[0].x = all->player_pos.x;
+    // all->img->instances[0].y = all->player_pos.y;
 }
 
 void move_right(t_all *all)
@@ -72,8 +72,8 @@ void move_right(t_all *all)
     if (all->map[(int)(all->player_pos.y / TILE_SIZE)][map_x] != '1')
         all->player_pos.x = next_x;
 
-    all->img->instances[0].x = all->player_pos.x;
-    all->img->instances[0].y = all->player_pos.y;
+    // all->img->instances[0].x = all->player_pos.x;
+    // all->img->instances[0].y = all->player_pos.y;
 }
 
 
