32e239be4c34ac719aa0be8023eab2b2c04b6ad9
Qrshh <adrienbesneux@outlook.fr>
2025-02-18 04:10:09 +0100

    Added welcome message when starting the game

--- src/main.c
+++ src/main.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mosmont <mosmont@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*   By: abesneux <abesneux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 18:20:31 by abesneux          #+#    #+#             */
-/*   Updated: 2025/02/17 22:21:43 by mosmont          ###   ########.fr       */
+/*   Updated: 2025/02/18 04:09:25 by abesneux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void welcome(void)
+{
+	ft_printf("\n\
+		\033[1;36m----------------------------------\n\
+			  Bienvenue dans Cub3D !\n\
+		----------------------------------\033[0m\n\
+		\033[1;33mCommandes :\033[0m\n\
+		\033[1;32m- Se déplacer :\033[0m WASD\n\
+		\033[1;32m- Diriger la caméra :\033[0m L and R arrows\n\
+		\033[1;32m- Allumer la flashlight :\033[0m F\n\
+		\033[1;32m- Diriger la caméra :\033[0m Souris (P pour activer/désactiver)\n\
+		\033[1;32m- Afficher la minimap :\033[0m (M pour acitver/desactiver)\n\
+		");		
+}
 
 void	escape(void *param)
 {
@@ -25,6 +40,7 @@ int	main(int ac, char **av)
 {
 	t_all	*all;
 
+	welcome();
 	parsing(&all, ac, av);
 	mlx_texture_t *txt = mlx_load_png("textures/arrow_minimap.png");
 	all->player_img = mlx_texture_to_image(all->mlx, txt);
